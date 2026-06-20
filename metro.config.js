442ecc444b460a53a6844120efcfff0f4c1d492c
saul9809 <slionking09@gmail.com>
2026-06-19 20:20:16 -0400

    Config ready

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// metro.config.js
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push("sql");
+
+module.exports = config;
